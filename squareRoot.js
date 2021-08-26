0084b54241e588f5f5859be356e3cc2156fed780
Adekunle Oduye <adekunleoduye@gmail.com>
2021-08-26 19:16:16 -0400

    doc: updated jsdoc

--- squareRoot.js
+++ squareRoot.js
@@ -1,10 +1,11 @@
 
 /**
- * Outputs square value
+ * Outputs square of num
+ * 
  * i.e. squareRoot(3) => 9
  * 
- * @param {int} num - Input number 
- * @returns 
+ * @param {number} num - Input number 
+ * @returns {number} Square root of num
  */
 export default function squareRoot(num) {
   return num ** 2
