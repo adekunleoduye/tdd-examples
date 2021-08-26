f927fc69f05f414808755cc86fef9a9bca0c7c0e
Adekunle Oduye <adekunleoduye@gmail.com>
2021-08-25 23:06:49 -0400

    feat: green test square root

---
+++ squareRoot.js
@@ -0,0 +1,11 @@
+
+/**
+ * Outputs square value
+ * i.e. squareRoot(3) => 9
+ * 
+ * @param {int} num - Input number 
+ * @returns 
+ */
+export default function squareRoot(num) {
+  return num * num
+}
