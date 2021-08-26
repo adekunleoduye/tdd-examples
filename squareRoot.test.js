d948501cf8fcf04523d293763afb28c7b9bb4e5f
Adekunle Oduye <adekunleoduye@gmail.com>
2021-08-26 08:11:56 -0400

    fix: typo

--- squareRoot.test.js
+++ squareRoot.test.js
@@ -2,9 +2,9 @@ import squareRoot from './squareRoot.js'
 
 describe('Squareroot', () => {
   it("Should output number's square", () => {
-    const acutal = squareRoot(3);
+    const actual = squareRoot(3);
     const expected = 9;
 
-    expect(acutal).toEqual(expected);
+    expect(actual).toEqual(expected);
   })
 })
